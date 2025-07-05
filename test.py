c2488290b66c12ea38bd1b3da312ab0a5f69e145
BraydenAC <ba.cloutier@yahoo.com>
2025-07-05 18:02:27 -0400

    Finished get_matching_posts, started on model loading and inference functionalities.

---
+++ test.py
@@ -0,0 +1,45 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import datasets as ds
+from huggingface_hub import login
+import os
+
+token = os.getenv('token')
+login(token)
+
+#load model and tokenizer
+print("Loading model and tokenizer...")
+model_name = "meta-llama/Llama-3.2-3B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+
+#Load in /shared/DATA/reddit/crowd/test/shared_task_posts_test.csv for post level features
+df_X = ds.load_dataset("csv", data_files="/shared/DATA/reddit/crowd/test/shared_task_posts_test.csv")['train']
+#Load in /shared/DATA/reddit/crowd/test/crowd_test.csv for user level labels
+df_y = ds.load_dataset("csv", data_files="/shared/DATA/reddit/crowd/test/crowd_test.csv")['train']
+separator = "\n\n"
+
+def get_matching_posts(user):
+    """
+    This function takes a dataset row from a y dataset, and stiches together data from both X and y into a user-level, matched dataset.
+
+    Args:
+        user (dict): A dictionary representing a row from the y dataset, containing 'user_id' and 'label'.
+    
+    Returns:
+        dict: All posts from a given user, concatenated into a single text string.
+    """
+    user_id = user['user_id']
+    matching_posts = df_X.filter(lambda row: row['user_id'] == user_id)
+    return {"text": separator.join([text for text in matching_posts['post_body'] if text is not None])}
+
+def get_predictions(input_row):
+    pass
+
+#Create a single dataset out of the key-value pairs of the original dataset
+print("Mapping user posts to their corresponding texts...")
+df = df_y.map(get_matching_posts, batched=False, desc="Mapping user posts")
+
+#Feedback on the mapping output
+print(f"Mapped dataset columns: {df.column_names}")
+print(f"Mapped dataset size: {len(df)}")
+
