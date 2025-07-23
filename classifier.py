5ac3e25a74aa0e984a71836ca1284494d63e2dc3
Brayden Cloutier <135390195+BraydenAC@users.noreply.github.com>
2025-07-23 09:50:27 -0400

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -10,7 +10,7 @@ login(token)
 
 #load model and tokenizer
 print("Loading model and tokenizer...")
-model_name = "meta-llama/Llama-3.1-8B-Instruct"
+model_name = "meta-llama/Llama-3.2-3B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token  # Set pad token to eos token for Llama models
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", pad_token_id=tokenizer.eos_token_id)
